5d5942b3261a399e78e1c5bc1c305b7b9faf4fde
Connor <107011324+connor33341@users.noreply.github.com>
2025-04-30 17:30:40 +0000

    update kvtypemap

--- kvprocessor/__init__.py
+++ kvprocessor/__init__.py
@@ -1,7 +1,7 @@
-__version__ = "0.2.1"
+__version__ = "0.2.14"
 
 from .kvprocessor import KVProcessor
-from .kvenvloader import LoadEnv
+from .kvenvloader import load_env, LoadEnv
 from .kvstructloader import KVStructLoader
 from .kvfileexporter import KVFileExporter
 from .kvfilemerger import KVFileMerger
@@ -9,4 +9,5 @@ from .kvdiff import KVFileDiffChecker
 from .kvvalidator import KVFileValidator
 from .kvversionmanager import KVVersionManager
 from .kvdiff import KVFileDiffChecker
+from .kvtypemap import get_type_map, set_type_map, remove_type_map, has_type_map, clear_type_map, add_type_map
 from .errors import KVProcessorError, InvalidKVFileError, MissingEnvironmentVariableError, NamespaceNotFoundError, InvalidNamespaceError
--- kvprocessor/kvenvloader.py
+++ kvprocessor/kvenvloader.py
@@ -1,8 +1,12 @@
 import os
 
-def LoadEnv(Names: list) -> dict[str, any]:
+def load_env(Names: list) -> dict[str, any]:
     EnvList = {}
     for Name in Names:
         Value = os.environ.get(Name)
         EnvList[Name] = Value
     return EnvList
+
+def LoadEnv(Names: list) -> dict[str, any]:
+    print("Using depricated function, update to load_env")
+    return load_env(Names)
